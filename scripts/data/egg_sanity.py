04a3912f4902460c63fa6cbf7f7b01a8b80de9c8
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-02-19 14:50:59 +0900

    Minor fix.

--- scripts/data/egg_sanity.py
+++ scripts/data/egg_sanity.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
 
 	scene = os.path.abspath('../../resources/examples/scenes/scene_00000001.egg')
 	riggs = glob('/home/yen/data/gern/egg_human/train/rigg*.egg')
-	anim = '/home/yen/data/gern/egg_human/train/anim_0000.egg'
-
+	
 	smgr.swapScene(scene)
 	for rigg in riggs:
+		anim = rigg.replace('rigg_', 'anim_').replace('.egg', '_0000.egg')
 		smgr.swapActor(rigg, anim, loop=True)
 		smgr.step()
