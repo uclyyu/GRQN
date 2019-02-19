607a68b1f981778ddc3822326f0f58dac7e0f686
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-02-20 08:40:53 +0900

    Minor change to how animations are sampled. Blender/Scene JSONs are no longer copied.

--- scripts/data/worker.py
+++ scripts/data/worker.py
@@ -1,5 +1,5 @@
 import multiprocessing as mp
-import queue, sys, argparse, json, scene, os, random, shutil
+import queue, sys, argparse, json, scene, os, random, shutil, glob
 
 
 def mp_collect(worker, serial, bjson, sjson, wsize, extension, output_base):
@@ -16,7 +16,7 @@ def mp_collect(worker, serial, bjson, sjson, wsize, extension, output_base):
 	sp_anim = os.path.abspath(sjson['animation_search_path'])
 	sp_pose = os.path.abspath(sjson['openpose_config'])
 
-	# --- List of actors and animations
+	# --- List of actors
 	actors = glob.glob(os.path.sep.join([sp_actr, 'rigg_*{}'.format(extension)]))
 	animations = glob.glob(os.path.sep.join([sp_anim, 'anim_*{}'.format(extension)]))
 
@@ -38,71 +38,79 @@ def mp_collect(worker, serial, bjson, sjson, wsize, extension, output_base):
 			print('Worker ({}): processing job {:,}.'.format(worker, job))
 			# Sample and generate .egg scene
 			blender.sample_environment(job, bp_flor, bp_wall, bp_blnd, bp_expo)
-			scene = os.path.sep.join([bp_expo, 'scene_{:08d}.egg'.format(job)])
+			scene_egg = os.path.sep.join([bp_expo, 'scene_{:08d}.egg'.format(job)])
 
 			# Sample actor and animation
-			actor, animation = _sample_actors(actors, animations)
+			actor, animation = _sample_actors(actors, sp_anim, extension)
 
 			# Update scene manager
-			output_path = os.path.sep.join(output_base, '{:08d}'.format(job))
-			smgr.swapScene(scene)
+			output_path = os.path.sep.join([output_base, '{:08d}'.format(job)])
+			smgr.swapScene(scene_egg)
 			smgr.swapActor(actor, animation)
 			smgr.rebase(output_path)
 			smgr.step()
 
 
-def _sample_actors(actors, animations):
+def _sample_actors(actors, anim_search_path, extension):
 	actor, = random.sample(actors, 1)
+	search_pattern = actor.split(os.path.sep)[-1].replace('rigg_', 'anim_').replace(extension, '_*' + extension)
+	search_path = os.path.sep.join([anim_search_path, search_pattern])
+	animations = glob.glob(search_path)
 	animation, = random.sample(animations, 1)
 	return actor, animation
 
 def _update_main_dict(main, args):
-	for k, v in args.items():
-		if k == 'bjson':
-			with open(v, 'r') as jr:
-				main.__dict__.update({k: json.load(jr)})
-		elif k == 'sjson':
-			with open(v, 'r') as jr:
-				main.__dict__.update({k: json.load(jr)})
-		elif k == 'wsize':
-			main.__dict__.update({k: tuple(map(int, v.split('x')))})
-		else:
-			main.__dict__.update({k: v})
+	if args['wjson'] is None:
+		output_dict = dict()
+		for k, v in args.items():
+			if k != 'wjson' and k is None:
+				raise
+
+			if k == 'bjson':
+				with open(v, 'r') as jr:
+					main.__dict__.update({k: json.load(jr)})
+			elif k == 'sjson':
+				with open(v, 'r') as jr:
+					main.__dict__.update({k: json.load(jr)})
+			elif k == 'wsize':
+				main.__dict__.update({k: tuple(map(int, v.split('x')))})
+			else:
+				main.__dict__.update({k: v})
+
+			output_dict.update({k: main.__dict__[k]})
+		return output_dict
+	else:
+		with open(args['wjson'], 'r') as jr:
+			args_json = json.load(jr)
+			main.__dict__.update(args_json)
+		return None
 
 
 if __name__ == '__main__':
 	main = sys.modules['__main__']
 
 	parser = argparse.ArgumentParser(description='Multi-worker data generator.')
-	parser.add_argument('--num-worker', type=int, dest='nworker', metavar='N', required=True, help='(integer) Number of multiprocessing workers.')
-	parser.add_argument('--num-sample', type=int, dest='nsample', metavar='M', required=True, help='(integer) Number of samples to draw.')
-	parser.add_argument('--file-extension', type=str, dest='extension', metavar='.EXT', required=True, default='.bam', help='')
-	parser.add_argument('--win-size', type=str, dest='wsize', metavar='WxH', required=True, default='128x128')
-	parser.add_argument('--out-base', type=str, dest='outpath', metavar='PATH', required=True, help='Root path for generated samples.')
-	parser.add_argument('--blender-json', type=str, dest='bjson', metavar='B', required=True, help='')
-	parser.add_argument('--scene-json', type=str, dest='sjson', metavar='S', required=True, help='')
+	parser.add_argument('--num-worker', 	type=int, dest='nworker', 	metavar='N', 					   help='Number of multiprocessing workers.')
+	parser.add_argument('--num-sample', 	type=int, dest='nsample', 	metavar='M', 					   help='Number of samples to draw.')
+	parser.add_argument('--file-extension', type=str, dest='extension', metavar='.EXT', default='.bam',	   help='Panda3D model extension.')
+	parser.add_argument('--win-size', 	  	type=str, dest='wsize', 	metavar='WxH',  default='128x128', help='Render window size.')
+	parser.add_argument('--out-base',	  	type=str, dest='outpath', 	metavar='PATH', 				   help='Root path for generated samples.')
+	parser.add_argument('--blender-json', 	type=str, dest='bjson', 	metavar='blender.json', 		   help='blender.py config file.')
+	parser.add_argument('--scene-json',   	type=str, dest='sjson', 	metavar='scene.json',  			   help='scene.py config file.')
+	parser.add_argument('--worker-json',  	type=str, dest='wjson', 	metavar='worker.json',			   help='JSON file to override command line arguments.')
 
 	arg_dict = vars(parser.parse_args())
-	_update_main_dict(main, arg_dict)
+	arg_dict_processed = _update_main_dict(main, arg_dict)
 
 	# Keep a copy of the arguments
 	configpath = os.path.sep.join([outpath, '../config'])
 	if not os.path.exists(configpath):
 		os.mkdir(configpath)
+	if arg_dict_processed:
+		with open(os.path.sep.join([configpath, 'worker_arguments.json']), 'w') as jw:
+			json.dump(arg_dict_processed, jw)
 
-	with open(os.path.sep.join(configpath, 'worker_arguments.json'), 'w') as jw:
-		json.dump(arg_dict, jw)
-
-	# Keep a copy of the JSON files
-	try:
-		shutil.copy(bjson, configpath)
-	except shutil.SameFileError:
-		print('Blender JSON already exists ... skipping.')
-	try:
-		shutil.copy(sjson, configpath)
-	except shutil.SameFileError:
-		print('Scene JSON already exists ... skipping.')
-	
+	# Main
 	if nworker > 1:
 		mp.set_start_method('spawn')
 
@@ -113,8 +121,8 @@ if __name__ == '__main__':
 
 		# Spawn workers
 		workers = []
-		for j in range(nworker):
-			args = (worker, serial, bjson, sjons, wsize, extension, outpath)
+		for nw in range(nworker):
+			args = (nw, serials, bjson, sjson, wsize, extension, outpath)
 			p = mp.Process(target=mp_collect, args=args)
 			workers.append(p)
 
@@ -127,7 +135,3 @@ if __name__ == '__main__':
 
 	else:
 		raise NotImplementedError
-
-
-
-
