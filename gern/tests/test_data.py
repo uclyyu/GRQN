1230f5286e52eb2dc046253be655b82b999b8abf
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-03-20 08:38:41 +0900

    Updated unit test.

--- gern/tests/test_data.py
+++ gern/tests/test_data.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 class TestGernDataset(unittest.TestCase):
 	def setUp(self):
 		self.dataset_cpu = data.GernDataset('resources/examples/dataset')
-		self.dataset_gpu = data.GernDataset('resources/examples/dataset', cuda=0)
 
 	def test_indexing_cpu(self):
 		cnd, qry, lab = self.dataset_cpu[0]
@@ -15,21 +14,15 @@ class TestGernDataset(unittest.TestCase):
 
 		self.assertEqual(Xc[0].size(), torch.Size([3, 256, 256]))
 
-
 		for cnd, qry, lab in self.dataset_cpu:
 			pass
-
-	def test_indexing_gpu(self):
-		pass
-	# 	cnd, qry, lab = self.dataset_gpu[0]
 
 
 class TestGernDataLoader(unittest.TestCase):
 	def setUp(self):
 		self.batch_size = 3
 		self.subset_size = 4
-		self.loader_cpu = data.GernDataLoader('resources/examples/dataset', cuda=None, subset_size=self.subset_size, drop_last=True, batch_size=self.batch_size)
-		self.loader_gpu = data.GernDataLoader('resources/examples/dataset', cuda=0, subset_size=self.subset_size, drop_last=True, batch_size=self.batch_size)
+		self.loader_cpu = data.GernDataLoader('resources/examples/dataset', subset_size=self.subset_size, drop_last=True, batch_size=self.batch_size)
 		self.num_batch = self.subset_size // self.batch_size
 
 	def test_draw_cpu(self):
@@ -37,9 +30,6 @@ class TestGernDataLoader(unittest.TestCase):
 			pass
 		self.assertEqual(i + 1, self.num_batch)
 		self.assertEqual(C[0].size(0), self.batch_size)
-
-	def test_draw_gpu(self):
-		pass
 
 
 if __name__ == '__main__':
