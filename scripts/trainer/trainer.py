1ce13f7b8b741f7b91148aaa44f0af1252522c9a
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-03-22 10:41:02 +0900

    Updated command line arguments. Minor fix.

--- scripts/trainer/trainer.py
+++ scripts/trainer/trainer.py
@@ -14,10 +14,11 @@ def initialise_process(args):
 		rank=args.rank,
 		world_size=args.world_size)
 
-def average_gradients(model):
-	world_size = float(distributed.get_world_size())
+def average_gradients(args, model):
+	group = distributed.new_group(range(args.world_size))
+	world_size = float(args.world_size)
 	for param in filter(lambda p: p.requires_grad, model.parameters()):
-		distributed.all_reduce(param.grad.data, op=distributed.reduce_op.SUM)
+		distributed.all_reduce(param.grad.data, op=distributed.ReduceOp.SUM, group=group)
 		param.grad.data /= world_size
 
 def log_best_json(args, epoch, accuracy, l_percept, l_heatmap, l_classifier, l_aggregate, l_kldiv):
@@ -51,7 +52,7 @@ def train_distributed(args, model, criterion, optimiser, lr_scheduler, sd_schedu
 				  'test':  GernDataLoader(args.rootdir_test,  subset_size=args.sample_size, batch_size=args.batch_size, num_workers=args.data_worker)}
 	
 	for epoch in range(args.from_epoch, args.total_epochs):
-
+		logger.opt(ansi=True).info('<cyan> ** Epoch {epoch}</cyan> **', epoch=epoch)
 		# alternating between training and testing phases
 		for phase in ['train', 'test']:
 			if phase == 'train':
@@ -89,7 +90,7 @@ def train_distributed(args, model, criterion, optimiser, lr_scheduler, sd_schedu
 
 			# --- Step optimiser (training phase only)
 			if phase == 'train':
-				average_gradients(model)
+				average_gradients(args, model)
 				optimiser.step()
 			
 			epoch_loss = running_loss / args.total_epochs
@@ -114,7 +115,8 @@ def train_distributed(args, model, criterion, optimiser, lr_scheduler, sd_schedu
 					logger.info('Created training checkpoint at epoch {epoch}.', epoch=epoch)
 
 			# --- Log training / testing progress
-			progress = {'phase': phase, 
+			progress = {'epoch': epoch,
+						'phase': phase, 
 						'l_percept': criterion.l_percept,
 						'l_heatmap': criterion.l_heatmap,
 						'l_classifier': criterion.l_classifier,
@@ -134,9 +136,15 @@ def main(args):
 	lr_scheduler = LearningRateScheduler(optimiser, args.lr_min, args.lr_max, args.lr_saturate_epoch)
 	sd_scheduler = PixelStdDevScheduler(args.criterion_weights, args.sd_min, args.sd_max, args.sd_saturate_epoch)
 
+	# --- Logger
+	logger_file = os.path.join(args.savedir, 'logger.txt')
+	logger.add(logger_file)
+
 	# --- Sanity checks.
-	if not os.path.exists(args.savedir):
-		os.makedirs(os.path.join(args.savedir, 'checkpoint'))
+	chkpdir = os.path.join(args.savedir, 'checkpoint')
+	if not os.path.exists(chkpdir):
+		logger.info('Adding new directory at {d}.', d=chkpdir)
+		os.makedirs(chkpdir)
 
 	# --- Get the latest epoch if forcing checkpoint.
 	if args.force_checkpoint:
@@ -150,7 +158,7 @@ def main(args):
 	if args.rank is None:
 		raise NotImplementedError
 	else:
-		csv_columns = ['phase', 'l_percept', 'l_heatmap', 'l_classifier', 'l_aggregate', 'l_kldiv', 'accuracy', 'is_best']
+		csv_columns = ['epoch', 'phase', 'l_percept', 'l_heatmap', 'l_classifier', 'l_aggregate', 'l_kldiv', 'accuracy', 'is_best']
 		csv_name = os.path.join(args.savedir, 'log_training.csv')
 		with open(csv_name, 'w') as csvfile:
 			csvwriter = csv.DictWriter(csvfile, fieldnames=csv_columns)
@@ -161,33 +169,33 @@ def main(args):
 if __name__ == '__main__':
 	UNDEFINED = '<UNDEFINED>'
 	parser = argparse.ArgumentParser()
-	# offer to generate default configuration file in json format. 
+	# Command line argument management
 	parser.add_argument('--use-json', type=str, default=UNDEFINED)
-	parser.add_argument('--generate-json', type=str, default=UNDEFINED)
-	# on distributed
+	parser.add_argument('--generate-default-json', type=str, default=UNDEFINED)
+	# Settings for PyTorch distributed module
 	parser.add_argument('--rank', type=int, default=0)
 	parser.add_argument('--world-size', type=int, default=1)
 	parser.add_argument('--backend', type=str, default='nccl')
 	parser.add_argument('--init-method', type=str, default='tcp://192.168.1.116:23456')
-	# on model
+	# Main training hyperparameters
 	parser.add_argument('--asteps', type=int, default=7)
-	parser.add_argument('--rsteps', default=None)
+	parser.add_argument('--max-rsteps', type=int, default=100)
 	parser.add_argument('--criterion-weights', nargs=5, type=float, default=[2.0, 1.0, 1.0, 1.0, 1.0])
-	# lr
 	parser.add_argument('--from-epoch', type=int, default=0)
-	parser.add_argument('--total-epochs', type=int, default=2e6)
+	parser.add_argument('--total-epochs', type=int, default=2000000)
 	parser.add_argument('--lr-min', type=float, default=5e-5)
 	parser.add_argument('--lr-max', type=float, default=5e-4)
-	parser.add_argument('--lr-saturate-epoch', type=int, default=1.6e6)
+	parser.add_argument('--lr-saturate-epoch', type=int, default=1600000)
 	parser.add_argument('--sd-min', type=float, default=0.7)
 	parser.add_argument('--sd-max', type=float, default=2.0)
-	parser.add_argument('--sd-saturate-epoch', type=int, default=2e5)
-	# load/save
+	parser.add_argument('--sd-saturate-epoch', type=int, default=200000)
+	# Dataset settings
 	parser.add_argument('--rootdir-train', type=str, default=UNDEFINED)
 	parser.add_argument('--rootdir-test', type=str, default=UNDEFINED)
 	parser.add_argument('--sample-size', type=int, default=64)
 	parser.add_argument('--batch-size', type=int, default=8)
 	parser.add_argument('--data-worker', type=int, default=4)
+	# Trained model and checkpoint output
 	parser.add_argument('--savedir', type=str, default=UNDEFINED)
 	parser.add_argument('--force-checkpoint', type=bool, default=False)
 	parser.add_argument('--checkpoint-interval', type=int, default=2000)
@@ -195,9 +203,10 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	# If generate-json is set, write arguments to file.
-	if os.path.exists(args.generate_json):
-		json_name = os.path.join(args.generate_json, 'trainer_arguments.json')
-		args.generate_json = None
+	if os.path.exists(args.generate_default_json):
+		json_name = os.path.join(args.generate_default_json, 'trainer_arguments.json')
+		logger.opt(ansi=True).info('Generate default .json at <cyan>{name}</cyan>.', name=json_name)
+		args.generate_default_json = None
 		with open(json_name, 'w') as jw:
 			json.dump(vars(args), jw)
 		sys.exit(0)
@@ -205,15 +214,12 @@ if __name__ == '__main__':
 	# If an argument .json file is assigned, update args.
 	if os.path.exists(args.use_json):
 		with open(args.use_json, 'r') as jr:
-			logger.info('Loading trainer arguments from {file}.', file=args.use_json)
 			json_data = json.load(jr)
+			json_data['use_json'] = args.use_json
 			args.__dict__.update(json_data)
+			logger.opt(ansi=True).info('Loaded trainer arguments from <cyan>{file}</cyan>.', file=args.use_json)
 
-	if args.savedir == UNDEFINED:
-		logger.exception('--savedir must be a valid path.')
+	if not os.path.exists(args.savedir):
+		logger.exception('--savedir "{path}" is not a valid path name.', path=args.savedir)
 
 	main(args)
-
-
-
-
