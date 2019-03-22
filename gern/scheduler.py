2836e2955c0dd2067176e1a4e44ab01386d43c9e
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-03-22 15:44:04 +0900

    Fixed a critical error on sd.

--- gern/scheduler.py
+++ gern/scheduler.py
@@ -1,3 +1,6 @@
+from loguru import logger
+
+
 class LearningRateScheduler(object):
 	def __init__(self, optimiser, lrmin, lrmax, end_epoch):
 		assert lrmin < lrmax
@@ -11,6 +14,7 @@ class LearningRateScheduler(object):
 		lr = max(lr, self.lrmin)
 		for param_group in self.optimiser.param_groups:
 			param_group['lr'] = lr
+		logger.opt(ansi=True).info('Stepping learning rate = {new:.8f}.', new=lr)
 
 
 class PixelStdDevScheduler(object):
@@ -26,4 +30,6 @@ class PixelStdDevScheduler(object):
 	def step(self, epoch):
 		sd = self.sdmin + (self.sdmax - self.sdmin) * (1 - epoch / self.end_epoch)
 		sd = max(sd, self.sdmin)
-		self.weights[self.index] = sd
+		self.weights[self.index] = 1. / sd
+		logger.opt(ansi=True).info('Stepping pixel-sd = {new:.8f}.', new=sd)
+
