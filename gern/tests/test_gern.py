93b15802f75efa40075edbda4f1c1fb7274cbef8
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-05-17 15:20:25 +0900

    Proper annotation and tests

--- gern/tests/test_gern.py
+++ gern/tests/test_gern.py
@@ -51,49 +51,6 @@ class TestRepresentationAggregator(unittest.TestCase):
 		self.assertEqual(self.net(x).size(), torch.Size([1, 256]))
 
 
-class TestAggregateRewind(unittest.TestCase):
-	def setUp(self):
-		self.net = gern.AggregateRewind(256, 128)
-		self.net_init = gern.AggregateRewind(256, 128, learn_init=True)
-
-	def test_optmiser(self):
-		optimiser = torch.optim.Adam(self.net.parameters(), 1e-3)
-		optimiser = torch.optim.Adam(self.net_init.parameters(), 1e-3)
-
-	def test_input(self):
-		N = 3
-		x = torch.randn(1, 256)
-		y, o = self.net(x, rewind_steps=N)
-		self.assertEqual(y.size(), torch.Size([1, N + 1, 256]))
-		self.assertEqual(o.size(), torch.Size([1, 128]))
-
-	def test_input_init(self):
-		N = 3
-		x = torch.randn(1, 256)
-		y, o = self.net_init(x, rewind_steps=N)
-		self.assertEqual(y.size(), torch.Size([1, N + 1, 256]))
-		self.assertEqual(o.size(), torch.Size([1, 128]))
-
-	def test_input_hco(self):
-		N = 3
-		x = torch.randn(1, 256)
-		h = torch.randn(1, 128)
-		c = torch.randn(1, 128)
-		o = torch.randn(1, 128)
-		y, o_hat = self.net(x, h, c, o, rewind_steps=N)
-		self.assertEqual(y.size(), torch.Size([1, N + 1, 256]))
-		self.assertEqual(o.size(), torch.Size([1, 128]))
-
-	def test_input_init_hco(self):
-		N = 3
-		x = torch.randn(1, 256)
-		h = torch.randn(1, 128)
-		c = torch.randn(1, 128)
-		o = torch.randn(1, 128)
-		y, o_hat = self.net_init(x, h, c, o, rewind_steps=N)
-		self.assertEqual(y.size(), torch.Size([1, N + 1, 256]))
-		self.assertEqual(o.size(), torch.Size([1, 128]))
-
 class TestRecurrentCell(unittest.TestCase):
 	def setUp(self):
 		self.net = gern.RecurrentCell(256, 128)
--- gern/tests/test_model.py
+++ gern/tests/test_model.py
@@ -13,6 +13,9 @@ class TestRepresentationEncoderState(unittest.TestCase):
 		self.net = model.RepresentationEncoderState(self.input_size, self.hidden_size, self.zoneout_prob)
 		# self.net_init = model.RepresentationEncoderState(input_size=256, hidden_size=128, zoneout=.15, learn_init=True)
 
+	def test_optmiser(self):
+		optimiser = torch.optim.Adam(self.net.parameters(), 1e-3)
+
 	def test_forward(self):
 		for dev in ['cpu', 'cuda']:
 			self.net.to(dev)
@@ -22,8 +25,39 @@ class TestRepresentationEncoderState(unittest.TestCase):
 			cel = torch.randn(hid.size(), device=dev)
 			pog = torch.randn(hid.size(), device=dev)
 
+			# with tensors
 			self.net(inp, hid, cel, pog)
 
+			# with nones
+			self.net(inp, None, None, None)
+
+
+class TestAggregateRewind(unittest.TestCase):
+	def setUp(self):
+		self.batch_size = 7
+		self.input_size = 256
+		self.sequence_length = 5
+		self.hidden_size = 512
+		self.zoneout_prob = 0.3
+		self.net = model.AggregateRewind(self.input_size, self.hidden_size, self.zoneout_prob)
+
+	def test_optmiser(self):
+		optimiser = torch.optim.Adam(self.net.parameters(), 1e-3)
+
+	def test_forward(self):
+		for dev in ['cpu', 'cuda']:
+			self.net.to(dev)
+
+			inp = torch.randn(self.batch_size, self.input_size, device=dev)
+			hid = torch.randn(self.batch_size, self.hidden_size, device=dev)
+			cel = torch.randn(hid.size(), device=dev)
+			pog = torch.randn(hid.size(), device=dev)
+
+			# with tensors
+			self.net(inp, hid, cel, pog, self.sequence_length)
+
+			# with Nones
+			self.net(inp, None, None, None, self.sequence_length)
 
 
 if __name__ == '__main__':
