c70d10c35b518047e3a0bbcedc488f2bb7bcb0ee
Yen Yu <yen.yu.10@ucl.ac.uk>
2019-10-21 14:58:25 +0900

    update for image weight.

--- scripts/has-data/worker.py
+++ scripts/has-data/worker.py
@@ -19,15 +19,30 @@ PI_2 = np.pi / 2
 PI = np.pi
 
 
+def get_binary_weight(img):
+    total_count = np.prod(img.shape)
+    one_count = img.sum()
+    zero_count = total_count - one_count
+
+    if one_count == 0 or zero_count == 0:
+        print('Invalid sample!')
+
+    a = total_count / (2 * one_count)
+    b = total_count / (2 * zero_count)
+    return a, b
+
+
 def task(basedir, mapsize, nrays, Z, camwh, scene, los):
     ctx_pos = None
     ctx_jlos = np.zeros(mapsize)
     df_ctx = pd.DataFrame(columns=['pos_x', 'pos_y', 'pos_z',
                                    'eul_x', 'eul_y', 'eul_z',
-                                   'file_pxl', 'file_dep', 'file_los', 'file_jlos'])
+                                   'file_pxl', 'file_dep', 'file_los', 'file_jlos',
+                                   'file_weight_jlos'])
     df_qry = pd.DataFrame(columns=['pos_x', 'pos_y', 'pos_z',
                                    'eul_x', 'eul_y', 'eul_z',
-                                   'file_pxl', 'file_dep', 'file_los'])
+                                   'file_pxl', 'file_dep', 'file_los',
+                                   'file_weight_los'])
     width, height = camwh
 
     for i in range(5):
@@ -47,18 +62,24 @@ def task(basedir, mapsize, nrays, Z, camwh, scene, los):
         img_pxl = cv2.cvtColor(img_pxl, cv2.COLOR_RGB2BGR)
         img_dep = cv2.normalize(img_dep, None, 0, 255, cv2.NORM_MINMAX)
 
+        img = _ctx_jlos / 255
+        u, v = get_binary_weight(img)
+        weight = np.where(img == 1, a, b)
+
         file_pxl = 'ctx-pxl-{:02d}.png'.format(i)
         file_dep = 'ctx-dep-{:02d}.png'.format(i)
         file_los = 'ctx-los-{:02d}.png'.format(i)
         file_jlos = 'ctx-jlos-{:02d}.png'.format(i)
+        file_weight = 'ctx-weight-jlos-{:02d}.npy'.format(i)
 
         cv2.imwrite(os.path.join(basedir, file_pxl), img_pxl)
         cv2.imwrite(os.path.join(basedir, file_dep), img_dep)
         cv2.imwrite(os.path.join(basedir, file_los), ctx_los)
         cv2.imwrite(os.path.join(basedir, file_jlos), _ctx_jlos)
+        np.save(os.path.join(basedir, file_weight), weight)
 
         df_ctx.loc[i] = [*scene.normalize_position(ctx_pos), *ctx_orn,
-                         file_pxl, file_dep, file_los, file_jlos]
+                         file_pxl, file_dep, file_los, file_jlos, file_weight]
 
         #
         # Query samples
@@ -76,8 +97,8 @@ def task(basedir, mapsize, nrays, Z, camwh, scene, los):
         qry_orn = scene.sample_orientation()
 
         qry_los = los.rays2map(qry_pos, qry_orn, nrays, scene.client)
-        qry_los = los.morphology(qry_los)
         qry_los = np.logical_and(qry_los, np.logical_not(ctx_jlos)) * 1.0
+        qry_los = los.morphology(qry_los)
         qry_los = cv2.normalize(qry_los, None, 0, 255, cv2.NORM_MINMAX)
 
         img_pxl, img_dep = scene.get_camera_image(
@@ -86,16 +107,22 @@ def task(basedir, mapsize, nrays, Z, camwh, scene, los):
         img_pxl = cv2.cvtColor(img_pxl, cv2.COLOR_RGB2BGR)
         img_dep = cv2.normalize(img_dep, None, 0, 255, cv2.NORM_MINMAX)
 
+        img = qry_los / 255
+        u, v = get_binary_weight(img)
+        weight = np.where(img == 1, a, b)
+
         file_pxl = 'qry-pxl-{:02d}.png'.format(i)
         file_dep = 'qry-dep-{:02d}.png'.format(i)
         file_los = 'qry-los-{:02d}.png'.format(i)
+        file_weight = 'qry-weight-los-{:02d}.npy'.format(i)
 
         cv2.imwrite(os.path.join(basedir, file_pxl), img_pxl)
         cv2.imwrite(os.path.join(basedir, file_dep), img_dep)
         cv2.imwrite(os.path.join(basedir, file_los), qry_los)
+        np.save(os.path.join(basedir, file_weight), weight)
 
         df_qry.loc[i] = [*scene.normalize_position(qry_pos), *qry_orn,
-                         file_pxl, file_dep, file_los]
+                         file_pxl, file_dep, file_los, file_weight]
 
     df_ctx.to_csv(os.path.join(basedir, 'ctx-data.csv'))
     df_qry.to_csv(os.path.join(basedir, 'qry-data.csv'))
